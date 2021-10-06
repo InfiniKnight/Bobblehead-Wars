ddade3247869c856a7e2a7295694800d680223ea
Devon Payette <djstu01@hotmail.ca>
2021-10-06 12:35:32 -0400

    chapter 6 Animating the Space Marine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
 	public Rigidbody head;
 	public LayerMask layerMask;
 	private Vector3 currentLookTarget = Vector3.zero;
+	public Animator bodyAnimator;
 	
     // Use this for initialization
 	void Start ()
@@ -29,11 +30,12 @@ public class PlayerController : MonoBehaviour
 		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		if (moveDirection == Vector3.zero)
         {
-			
+			bodyAnimator.SetBool("IsMoving", false);
         }
 		else
         {
 			head.AddForce(transform.right * 150, ForceMode.Acceleration);
+			bodyAnimator.SetBool("IsMoving", true);
         }
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
